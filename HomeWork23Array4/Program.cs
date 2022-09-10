e9ef9c304b020e1488971568ce2ca970481ec9b7
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-09-10 22:22:18 +0300

    Fixed

--- HomeWork23Array4/Program.cs
+++ HomeWork23Array4/Program.cs
@@ -16,8 +16,6 @@
             Console.Write($"Введите число: ");
             string userInput = Console.ReadLine();
 
-            int[] tempArray = new int[array.Length + 1];
-
             if (userInput.ToLower() == Sum.ToLower())
             {
                 for (int i = 0; i < array.Length; i++)
@@ -29,15 +27,15 @@
 
                 finalSum = 0;
             }
-
-            if (userInput.ToLower() == Exit.ToLower())
+            else if (userInput.ToLower() == Exit.ToLower())
             {
                 isWorking = false;
             }
-
-            if (userInput.ToLower() != Sum.ToLower() && userInput.ToLower() != Exit.ToLower())
+            else if (userInput.ToLower() != Sum.ToLower() && userInput.ToLower() != Exit.ToLower())
             {
                 number = Convert.ToInt32(userInput);
+
+                int[] tempArray = new int[array.Length + 1];
 
                 for (int i = 0; i < array.Length; i++)
                 {
